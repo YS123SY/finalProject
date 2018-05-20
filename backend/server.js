ebf7dded3860aa9536f25404ce0f9a54f8b663c9
YS123SY <yosragueffrech2017@gmail.com>
2018-05-20 12:57:19 +0200

    chae

--- backend/server.js
+++ backend/server.js
@@ -7,16 +7,16 @@ const app = express();
 app.use(bodyParser.json());
 
 const mongo_url = "mongodb://localhost:27017";
-const database = "articles"; //contact
+const database = "articles";
 
 MongoClient.connect(mongo_url, (err, client) => {
   assert.equal(null, err, "data-base error");
 
-  const db = article.db(database); //client
+  const db = client.db(database);
 
-  app.post("/", (req, res) => {
-    let new_article = req.body;
-    db.collection("arc").insertOne({ ...new_article }, (err, data) => {
+  app.post("/post", (req, res) => {
+    let new_articles = req.body;
+    db.collection("arc").insertOne({ ...new_articles }, (err, data) => {
       if (err) {
         res.send("cant add the new article");
       } else {
@@ -24,54 +24,13 @@ MongoClient.connect(mongo_url, (err, client) => {
       }
     });
   });
-
   app.get("/", (req, res) => {
     db
       .collection("arc")
       .find()
       .toArray((err, data) => {
-        res.send(data);
+        res.send("ffffffffffff");
       });
-  });
-
-  app.get("/:id", (req, res) => {
-    const id = ObjectID(req.params.id);
-    db.collection("arc").findOne({ _id: id }, (err, data) => {
-      if (err) {
-        res.send("cant get the requested article");
-      } else {
-        res.send(data);
-      }
-    });
-  });
-
-  app.delete("/:id", (req, res) => {
-    const id = ObjectID(req.params.id);
-    db.collection("arc").findOneAndDelete({ _id: id }, (err, data) => {
-      if (err) {
-        res.send("cant delete the requested article");
-      } else {
-        res.send("article removed");
-      }
-    });
-  });
-
-  app.put("/:id", (req, res) => {
-    const id = ObjectID(req.params.id);
-    const updateInfo = req.body;
-    db
-      .collection("arc")
-      .findOneAndUpdate(
-        { _id: id },
-        { $set: { ...updateInfo } },
-        (err, data) => {
-          if (err) {
-            res.send("cant updated the requested article");
-          } else {
-            res.send("article updated");
-          }
-        }
-      );
   });
 });
 
